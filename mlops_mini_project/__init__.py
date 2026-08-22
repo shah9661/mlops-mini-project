2050a83e70783db368afeacb6aba7cd97f4ba6a5
shah9661 <shanshadalam96@gmail.com>
2026-08-22 10:54:02 +0530

    Initail commit

---
+++ mlops_mini_project/__init__.py
@@ -0,0 +1,1 @@
+from mlops_mini_project import config  # noqa: F401
---
+++ notebooks/dagshub_setup.py
@@ -0,0 +1,9 @@
+import mlflow
+import dagshub
+
+dagshub.init(repo_owner='shanshad0999', repo_name='mlops-mini-project', mlflow=True)
+mlflow.set_tracking_uri('https://dagshub.com/shanshad0999/mlops-mini-project.mlflow')
+
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
